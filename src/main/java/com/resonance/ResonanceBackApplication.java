4fcff876e72d42c92e0b699162d81981cace654f
AnthonyR <anthony156gamer@gmail.com>
2026-01-07 16:20:29 +0100

    feat: init project

---
+++ src/main/java/com/resonance/ResonanceBackApplication.java
@@ -0,0 +1,13 @@
+package com.resonance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResonanceBackApplication {
+
+    void main(String[] args) {
+        SpringApplication.run(ResonanceBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/resonance/ResonanceBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resonance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResonanceBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
