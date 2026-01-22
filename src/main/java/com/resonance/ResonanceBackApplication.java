7f0eb24a3f8c6fc3cb25aa841ae2065659885278
AnthonyR <anthony156gamer@gmail.com>
2026-01-22 22:47:24 +0100

    feat: implement rate limiter for all endpoints

--- src/main/java/com/resonance/ResonanceBackApplication.java
+++ src/main/java/com/resonance/ResonanceBackApplication.java
@@ -1,13 +1,24 @@
 package com.resonance;
 
+import com.resonance.components.RateLimiter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ResonanceBackApplication {
 
     static void main(String[] args) {
         SpringApplication.run(ResonanceBackApplication.class, args);
+    }
+
+    @Bean
+    public FilterRegistrationBean<RateLimiter> rateLimitingFilter() {
+        FilterRegistrationBean<RateLimiter> registration = new FilterRegistrationBean<>();
+        registration.setFilter(new RateLimiter());
+        registration.addUrlPatterns("/api/*");
+        return registration;
     }
 
 }
